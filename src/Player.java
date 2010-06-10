27a71c807a89a39b201e13272c779399b74c16d2
kerplatz <kerplatz@ab2cb2b9-6a62-2b69-ef9e-a5780dfda81e>
2010-06-10 22:11:06 +0000

    Minor changes.

--- src/Player.java
+++ src/Player.java
@@ -102,44 +102,59 @@ public class Player {
 		if (tricksTemp > bidTemp) {
 			//When a nil or double nil bid.
 			if (bidTemp == 0) {
-			
-			
-			
+				//Nil bid.
+				if (bid.equals("nil")) {
+					//Deal with the score.
+					scoreTemp -= Main.nilValueNumb;
+				}
+				if (bid.equals("dbl")) {
+					//Deal with the score.
+					scoreTemp -= Main.doubleNilValueNumb;
+				}
+				
+				//The player was not set.
+				set = true;
 			}
 			//When not a nil or double nil bid.	
 			else {
-				//Deal with bags.
-				bagsRecvd += tricksTemp - bidTemp;
-				scoreTemp += bagsRecvd * Main.bagValueNumb;
-				bagsTotal = bagsTemp + bagsRecvd;
-
 				//Deal with score.
 				scoreTemp += bidTemp * 10;
 			
 				//The player was not set.
 				set = false;
 			}
+			
+			//Deal with bags.
+			bagsRecvd += tricksTemp - bidTemp;
+			scoreTemp += bagsRecvd * Main.bagValueNumb;
+			bagsTotal = bagsTemp + bagsRecvd;
 		}
 		
 		//Calculate the preliminary score when tricks taken equals bid.
 		if (tricksTemp == bidTemp) {
 			//When a nil or double nil bid.
 			if (bidTemp == 0) {
-
-				
-				
+				//Nil bid.
+				if (bid.equals("nil")) {
+					//Deal with the score.
+					scoreTemp += Main.nilValueNumb;
+				}
+				if (bid.equals("dbl")) {
+					//Deal with the score.
+					scoreTemp += Main.doubleNilValueNumb;
+				}
 			}
 			//When not a nil or double nil bid.
 			else {
 				//Deal with the score.
 				scoreTemp += bidTemp * 10;
-				
-				//Save the bags.
-				bagsTotal = bagsTemp;
-
-				//The player was not set.
-				set = false;
 			}
+			
+			//Save the bags.
+			bagsTotal = bagsTemp;
+
+			//The player was not set.
+			set = false;
 		}
 		
 		//Calculate the preliminary score when tricks taken is less than bid.
@@ -178,7 +193,7 @@ public class Player {
 		int numb = 0;
 		
 		//Search and record the number of times 'true' is found.
-		for (int i = 0; i <= turn.size(); i++) {
+		for (int i = 0; i < turn.size(); i++) {
 			if (turn.get(i).equals("true")) numb++;
 		}
 		
@@ -206,7 +221,7 @@ public class Player {
 		boolean flag = true;
 		int start = Utils.stringToInt(turn.get(0));
 				
-		for (int i = start; i <= turn.size() - 1; i += 6) {
+		for (int i = start; i < turn.size(); i += 6) {
 			for (int j = 0; j < 6; j++) {
 				//Get the desired item from the ArrayList.
 				if (flag) {
