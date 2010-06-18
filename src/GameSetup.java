a4862b68a1e07ba8a38770d332e43215ba7c729d
kerplatz <kerplatz@ab2cb2b9-6a62-2b69-ef9e-a5780dfda81e>
2010-06-18 16:55:34 +0000

    Minor changes.

--- src/GameSetup.java
+++ src/GameSetup.java
@@ -32,13 +32,17 @@ import java.awt.Label;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class GameSetup extends Frame implements ActionListener {
+public class GameSetup extends Frame implements ActionListener, ItemListener {
 	
     /**
 	 * Declare needed variables.
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	boolean hasPlayerChanged = false;
 
 	static Choice choiceBoxPlayer1;
 	static Choice choiceBoxPlayer2;
@@ -57,6 +61,11 @@ public class GameSetup extends Frame implements ActionListener {
 	static Checkbox player2IsDealer = new Checkbox("Player 2", dealerGroup, false);
 	static Checkbox player3IsDealer = new Checkbox("Player 3", dealerGroup, false);
 	static Checkbox player4IsDealer = new Checkbox("Player 4", dealerGroup, false);
+
+	static Checkbox player1Checkbox = new Checkbox("Change", false);
+	static Checkbox player2Checkbox = new Checkbox("Change", false);
+	static Checkbox player3Checkbox = new Checkbox("Change", false);
+	static Checkbox player4Checkbox = new Checkbox("Change", false);
 
 	Label player1Label = new Label("  Player 1  ");
 	Label player2Label = new Label("  Player 2  ");
@@ -135,15 +144,32 @@ public class GameSetup extends Frame implements ActionListener {
 
         //Performs this action when the ClearPlayers button is pressed.
         if (event.getActionCommand().equals("clearPlayers")) {
-        	frame.removeAll();
         	Utils.clearPlayerNames();
-        	createSetupPlayersScreen();
         }  
 
         //Performs this action when the ReturnPlayers button is pressed.
         if (event.getActionCommand().equals("returnPlayers")) {
         	if (Utils.isPlayersSetupDone()) {
-            	frame.removeAll();
+            	if (hasPlayerChanged) {
+            		//Determine which player was changed.
+            		if (player1Checkbox.getState()) {
+            			Player temp = new Player(Main.player1);
+            			Utils.changePlayer(Main.playerOne, temp, 1);
+            		}
+            		if (player2Checkbox.getState()) {
+            			Player temp = new Player(Main.player2);
+            			Utils.changePlayer(Main.playerTwo, temp, 2);
+            		}
+            		if (player3Checkbox.getState()) {
+            			Player temp = new Player(Main.player3);
+            			Utils.changePlayer(Main.playerThree, temp, 3);
+            		}
+            		if (player4Checkbox.getState()) {
+            			Player temp = new Player(Main.player4);
+            			Utils.changePlayer(Main.playerFour, temp, 4);
+            		}
+            	}
+        		frame.removeAll();
             	createSetupScreen();
         	}
         }  
@@ -186,6 +212,89 @@ public class GameSetup extends Frame implements ActionListener {
         	Main game = new Main();
         	game.createMainMenuScreen();
         }  
+	}
+
+	/**
+	 * 
+	 * @param event
+	 */
+    public void itemStateChanged(ItemEvent event) {
+    	if (event.getItemSelectable().equals(player1Checkbox)) {
+			if (player1Checkbox.getState()) {
+				player2Checkbox.setEnabled(false);
+				player3Checkbox.setEnabled(false);
+				player4Checkbox.setEnabled(false);
+				choiceBoxPlayer2.setEnabled(false);
+				choiceBoxPlayer3.setEnabled(false);
+				choiceBoxPlayer4.setEnabled(false);
+				hasPlayerChanged = true;
+			} else {
+				player2Checkbox.setEnabled(true);
+				player3Checkbox.setEnabled(true);
+				player4Checkbox.setEnabled(true);
+				choiceBoxPlayer2.setEnabled(true);
+				choiceBoxPlayer3.setEnabled(true);
+				choiceBoxPlayer4.setEnabled(true);
+				hasPlayerChanged = false;
+			}
+    	}
+    	if (event.getItemSelectable().equals(player2Checkbox)) {
+			if (player2Checkbox.getState()) {
+				player1Checkbox.setEnabled(false);
+				player3Checkbox.setEnabled(false);
+				player4Checkbox.setEnabled(false);
+				choiceBoxPlayer1.setEnabled(false);
+				choiceBoxPlayer3.setEnabled(false);
+				choiceBoxPlayer4.setEnabled(false);
+				hasPlayerChanged = true;
+			} else {
+				player1Checkbox.setEnabled(true);
+				player3Checkbox.setEnabled(true);
+				player4Checkbox.setEnabled(true);
+				choiceBoxPlayer1.setEnabled(true);
+				choiceBoxPlayer3.setEnabled(true);
+				choiceBoxPlayer4.setEnabled(true);
+				hasPlayerChanged = false;
+			}
+    	}
+    	if (event.getItemSelectable().equals(player3Checkbox)) {
+			if (player3Checkbox.getState()) {
+				player1Checkbox.setEnabled(false);
+				player2Checkbox.setEnabled(false);
+				player4Checkbox.setEnabled(false);
+				choiceBoxPlayer1.setEnabled(false);
+				choiceBoxPlayer2.setEnabled(false);
+				choiceBoxPlayer4.setEnabled(false);
+				hasPlayerChanged = true;
+			} else {
+				player1Checkbox.setEnabled(true);
+				player2Checkbox.setEnabled(true);
+				player4Checkbox.setEnabled(true);
+				choiceBoxPlayer1.setEnabled(true);
+				choiceBoxPlayer2.setEnabled(true);
+				choiceBoxPlayer4.setEnabled(true);
+				hasPlayerChanged = false;
+			}
+    	}
+    	if (event.getItemSelectable().equals(player4Checkbox)) {
+			if (player4Checkbox.getState()) {
+				player1Checkbox.setEnabled(false);
+				player2Checkbox.setEnabled(false);
+				player3Checkbox.setEnabled(false);
+				choiceBoxPlayer1.setEnabled(false);
+				choiceBoxPlayer2.setEnabled(false);
+				choiceBoxPlayer3.setEnabled(false);
+				hasPlayerChanged = true;
+			} else {
+				player1Checkbox.setEnabled(true);
+				player2Checkbox.setEnabled(true);
+				player3Checkbox.setEnabled(true);
+				choiceBoxPlayer1.setEnabled(true);
+				choiceBoxPlayer2.setEnabled(true);
+				choiceBoxPlayer3.setEnabled(true);
+				hasPlayerChanged = false;
+			}
+    	}
 	}
 
 	/**
@@ -326,17 +435,54 @@ public class GameSetup extends Frame implements ActionListener {
 		//Add player labels and text fields.
 		middlePanel.setLayout(new GridBagLayout());
 		middlePanel.add(player1Label, FrameUtils.gbLayoutNormal(0, 0));
-	    middlePanel.add(player2Label, FrameUtils.gbLayoutNormal(0, 1));
-		middlePanel.add(player3Label, FrameUtils.gbLayoutNormal(0, 2));
-
 		middlePanel.add(choiceBoxPlayer1, FrameUtils.gbLayoutNormal(1, 0));
+	    middlePanel.add(player2Label, FrameUtils.gbLayoutNormal(0, 1));
 		middlePanel.add(choiceBoxPlayer2, FrameUtils.gbLayoutNormal(1, 1));
+		middlePanel.add(player3Label, FrameUtils.gbLayoutNormal(0, 2));
 		middlePanel.add(choiceBoxPlayer3, FrameUtils.gbLayoutNormal(1, 2));
+
+		//Hide the check boxes if the game has not been started.
+		if (Main.isGameStarted) {
+			middlePanel.add(player2Checkbox, FrameUtils.gbLayoutNormal(2, 1));
+			player2Checkbox.addItemListener(this);
+			middlePanel.add(player1Checkbox, FrameUtils.gbLayoutNormal(2, 0));
+			player1Checkbox.addItemListener(this);
+			middlePanel.add(player3Checkbox, FrameUtils.gbLayoutNormal(2, 2));
+			player3Checkbox.addItemListener(this);
+			
+			//Disable players if the clear button was pressed.
+			if (player1Checkbox.getState()) {
+				choiceBoxPlayer2.setEnabled(false);
+				choiceBoxPlayer3.setEnabled(false);
+				choiceBoxPlayer4.setEnabled(false);
+			}
+			if (player2Checkbox.getState()) {
+				choiceBoxPlayer1.setEnabled(false);
+				choiceBoxPlayer3.setEnabled(false);
+				choiceBoxPlayer4.setEnabled(false);
+			}
+			if (player3Checkbox.getState()) {
+				choiceBoxPlayer1.setEnabled(false);
+				choiceBoxPlayer2.setEnabled(false);
+				choiceBoxPlayer4.setEnabled(false);
+			}
+			if (player4Checkbox.getState()) {
+				choiceBoxPlayer1.setEnabled(false);
+				choiceBoxPlayer2.setEnabled(false);
+				choiceBoxPlayer3.setEnabled(false);
+			}
+		}
 		
 		//Don't show fourth player if playing 3 handed game.
 		if (!Main.isThreeHanded) {
 			middlePanel.add(player4Label, FrameUtils.gbLayoutNormal(0, 3));
 			middlePanel.add(choiceBoxPlayer4, FrameUtils.gbLayoutNormal(1, 3));
+			
+			//Hide the check boxes if the game has not been started.
+			if (Main.isGameStarted) {
+				middlePanel.add(player4Checkbox, FrameUtils.gbLayoutNormal(2, 3));
+				player4Checkbox.addItemListener(this);
+			}
 		}
 		
 		//Shows the previous players, if there were any.
